89546ad91a0e07c6b4216a3163df8d7a1d2d1b61
tshen-dev <boo.fatup@gmail.com>
2023-01-18 20:34:13 +0700

    update repo

---
+++ services/my-pet-service-template/src/main/java/com/mypet/template/MyPetServiceTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.mypet.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetServiceTemplateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyPetServiceTemplateApplication.class, args);
+  }
+
+}
---
+++ services/my-pet-utils/src/main/java/com/mypet/utils/MyPetUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.mypet.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPetUtilsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyPetUtilsApplication.class, args);
+  }
+
+}
