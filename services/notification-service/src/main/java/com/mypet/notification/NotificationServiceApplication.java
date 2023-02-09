dfd0dd2072323bdd54a3584e677048d23cd285f3
tshen-dev <boo.fatup@gmail.com>
2023-02-09 12:22:08 +0700

    add notification service, update ignore files and cloud config

---
+++ services/notification-service/src/main/java/com/mypet/notification/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mypet.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NotificationServiceApplication.class, args);
+  }
+
+}
